c899c15ccd83d39401688123fd6a0e65c5302ab1
jingliancui <ericcui@qq.com>
2020-04-22 14:22:59 +0800

    delete obj and bin

--- Android/com.aliyun.iot.aep.sdk/abus/0.0.1/ABusBinding/ABusBinding/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,2 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.aliyun.iot.aep.sdk.abus", Managed="Com.Aliyun.Iot.Aep.Sdk.Abus")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.aliyun.iot.aep.sdk.abus.utils", Managed="Com.Aliyun.Iot.Aep.Sdk.Abus.Utils")]
