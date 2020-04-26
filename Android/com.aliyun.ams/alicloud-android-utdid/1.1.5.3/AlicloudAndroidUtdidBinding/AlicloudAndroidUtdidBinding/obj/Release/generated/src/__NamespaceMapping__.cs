85f86f3c3e00b2269135c969bb8cd0c6aaf9d0fa
丁川 <2505111990@qq.com>
2020-04-26 12:38:04 +0800

    add oss-android-sdk 2.7.0

--- Android/com.aliyun.ams/alicloud-android-utdid/1.1.5.3/AlicloudAndroidUtdidBinding/AlicloudAndroidUtdidBinding/obj/Release/generated/src/__NamespaceMapping__.cs
+++
@@ -1,2 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ta.utdid2.device", Managed="Com.TA.Utdid2.Device")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.ut.device", Managed="Com.UT.Device")]
---
+++ Android/com.aliyun.dpa/oss-android-sdk/2.7.0/OssAndroidSdkBinding/OssAndroidSdkBinding/Additions/MultipartUploadTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Alibaba.Sdk.Android.Oss.Internal
+{
+    public partial class MultipartUploadTask
+    {
+        //protected override Java.Lang.Object DoMultipartUpload()
+        //{
+
+        //}
+    }
+}
