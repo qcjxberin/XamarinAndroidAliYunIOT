2c471a7593b3e9fa1ec4890e36d40694b0b80614
丁川 <2505111990@qq.com>
2020-04-26 11:31:54 +0800

    add uicomponent 0.1.0

---
+++ Android/com.aliyun.iot.link/uicomponent/0.1.0/UiComponentBinding/UiComponentBinding/Additions/WheelView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Aliyun.Iot.Link.UI.Component
+{
+    public partial class WheelView
+    {
+        public override bool Selected
+        {
+            get
+            {
+                return RawSelected() == 1;
+            }
+        }
+    }
+}
