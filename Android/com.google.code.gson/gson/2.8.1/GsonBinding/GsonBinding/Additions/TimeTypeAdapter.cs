fea5139d859d0b76931cbb6e08c9fe661eceb950
丁川 <2505111990@qq.com>
2020-05-16 16:53:10 +0800

    add Gson

---
+++ Android/com.google.code.gson/gson/2.8.1/GsonBinding/GsonBinding/Additions/TimeTypeAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Google.Gson.Stream;
+
+namespace Com.Google.Gson.Internal.Bind
+{
+    public sealed partial class TimeTypeAdapter : global::Com.Google.Gson.TypeAdapter
+    {
+        public override Java.Lang.Object Read(JsonReader p0)
+        {
+            return RawRead(p0);
+        }
+
+        public override void Write(JsonWriter p0, Java.Lang.Object p1)
+        {
+            RawWrite(p0, (Java.Sql.Time)p1);
+        }
+    }
+}
