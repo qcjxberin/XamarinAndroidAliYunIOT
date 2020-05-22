e15d9bbd5987ffd59ea528630e4dd088d2ef88da
jingliancui <ericcui@qq.com>
2020-05-22 11:18:03 +0800

    upload lpbs-plugin-breeze binding project

---
+++ Android/com.aliyun.alink.linksdk/lpbs-plugin-breeze/1.1.0.1/LpbsPluginBreezeBinding/LpbsPluginBreezeBinding/Additions/Bridge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Aliyun.Alink.Linksdk.Alcs.Lpbs.Bridge;
+using Com.Aliyun.Alink.Linksdk.Alcs.Lpbs.Data.Group;
+
+namespace Com.Aliyun.Iot.Breeze.Lpbs
+{
+    public partial class Bridge
+    {
+        public IPalGroupConnect GetGroupConnect(PalGroupInfo p0) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
