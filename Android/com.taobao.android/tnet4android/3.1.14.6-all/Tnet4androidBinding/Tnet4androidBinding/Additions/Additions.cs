e30b2821e8d53c78ec34034d81fcaac61fcd1042
jingliancui <ericcui@qq.com>
2020-05-27 17:21:10 +0800

    upload tnet4android binding project

---
+++ Android/com.taobao.android/tnet4android/3.1.14.6-all/Tnet4androidBinding/Tnet4androidBinding/Additions/Additions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Org.Android.Spdy
+{
+    public partial class SpdyByteArray 
+    {
+        public int CompareTo(Java.Lang.Object o) 
+        {
+            return CompareTo(o as SpdyByteArray);
+        }
+    }
+}
